d83ca9f8ed257ab884b13039ba77a06fb017f50a
otavioluism <otavioluism97@hotmail.com>
2024-10-14 22:03:52 -0300

    adicionando validação nos campos da entidade candidato

--- src/main/java/br/com/otavioluism/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/otavioluism/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -1,6 +1,9 @@
 package br.com.otavioluism.gestao_vagas.modules.candidate;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 import java.util.UUID;
 
@@ -8,11 +11,20 @@ import java.util.UUID;
 public class CandidateEntity {
 
     private UUID id;
+
     private String name;
+
+    @Pattern(regexp = "^(?!\\s*$).+", message = "O campo [username] não deve conter espaços")
     private String username;
+
+    @Email(message = "O campo [email] deve conter um e-mail válido")
     private String email;
+
+    @Length(min = 10, max = 100) // minimo de 10 e maximo 100 caractereres para a senha
     private String password;
+
     private String description;
+
     private String curriculum;
 
 }
--- src/main/java/br/com/otavioluism/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/otavioluism/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -2,13 +2,14 @@ package br.com.otavioluism.gestao_vagas.modules.candidate.controllers;
 
 import br.com.otavioluism.gestao_vagas.modules.candidate.CandidateEntity;
 import org.springframework.web.bind.annotation.*;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/candidate")
 public class CandidateController {
 
     @PostMapping("/")
-    public void create(@RequestBody CandidateEntity candidateEntity) {
+    public void create(@Valid @RequestBody CandidateEntity candidateEntity) {
         System.out.println("Candidato...");
         System.out.println(candidateEntity.getEmail());
 
