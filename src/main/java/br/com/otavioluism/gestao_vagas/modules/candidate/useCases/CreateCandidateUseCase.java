496ef754fa3d7d9b37bc6065c03c3d3cee34fc59
otavioluism <otavioluism97@hotmail.com>
2024-10-23 08:51:03 -0300

    Adicionando uma forma de realiazar a criptografia das senhas salvas no banco de dados

--- src/main/java/br/com/otavioluism/gestao_vagas/modules/candidate/useCases/CreateCandidateUseCase.java
+++ src/main/java/br/com/otavioluism/gestao_vagas/modules/candidate/useCases/CreateCandidateUseCase.java
@@ -4,6 +4,7 @@ import br.com.otavioluism.gestao_vagas.modules.candidate.CandidateEntity;
 import br.com.otavioluism.gestao_vagas.modules.candidate.CandidateRepository;
 import br.com.otavioluism.gestao_vagas.exceptions.UserFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service // anotation para marcar classe como camada de serviço
@@ -12,12 +13,18 @@ public class CreateCandidateUseCase {
     @Autowired // utilizando esta anotation para o spring gerenciar sua construcao
     private CandidateRepository candidateRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public CandidateEntity execute(CandidateEntity candidateEntity) {
         this.candidateRepository
                 .findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail())
                 .ifPresent((user) -> {
                     throw new UserFoundException();
                 });
+
+        var password = passwordEncoder.encode(candidateEntity.getPassword());
+        candidateEntity.setPassword(password);
 
         return this.candidateRepository.save(candidateEntity);
     }
