4afc42cd3fc17783cbd296cea46d9373d8c26964
otavioluism <otavioluism97@hotmail.com>
2024-10-31 08:51:55 -0300

    Criando um componente de filtro que toda requisição passa por ele e este filtro tem como responsabilidade de deixar passar a requisição ou não, ele que será quem demanda como retorna ao cliente

--- src/main/java/br/com/otavioluism/gestao_vagas/security/SecurityConfig.java
+++ src/main/java/br/com/otavioluism/gestao_vagas/security/SecurityConfig.java
@@ -1,14 +1,19 @@
 package br.com.otavioluism.gestao_vagas.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
+
+    @Autowired
+    private SecurityFilter securityFilter;
 
     @Bean // anotation reponsavel por subscrever os metodos
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {// metodo responsavel por controlar os endpoints
@@ -18,7 +23,9 @@ public class SecurityConfig {
                             .requestMatchers("/company/").permitAll()
                             .requestMatchers("/auth/company").permitAll();
                     auth.anyRequest().authenticated(); // qualquer outros rotas autenticar
-                });
+                })
+                .addFilterBefore(securityFilter, BasicAuthenticationFilter.class);
+        ;
         return http.build();
     }
 
