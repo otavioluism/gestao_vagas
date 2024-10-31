236d745b8641d527b06e15fffcfe42f833d0a8fb
otavioluism <otavioluism97@hotmail.com>
2024-10-31 09:21:51 -0300

    Removendo o campo passado como company_id pela requisição body e captando do token e o componente filter liberando para a requisição

---
+++ src/main/java/br/com/otavioluism/gestao_vagas/providers/JWTProvider.java
@@ -0,0 +1,31 @@
+package br.com.otavioluism.gestao_vagas.providers;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JWTProvider {
+
+    @Value("${secret.token.security}")
+    private String secretKey;
+
+    public String validateToken(String token) {
+        token = token.replace("Bearer ", "");
+
+        Algorithm algorithm = Algorithm.HMAC256(secretKey);
+
+        try {
+            var subject = JWT.require(algorithm)
+                    .build()
+                    .verify(token)
+                    .getSubject(); // pegando pelo subject por conta de inserirmos o Id da empresa dentro do token
+            return subject;
+        } catch (JWTVerificationException ex)  {
+            ex.printStackTrace();
+            return "";
+        }
+    }
+}
--- src/main/java/br/com/otavioluism/gestao_vagas/security/SecurityFilter.java
+++ src/main/java/br/com/otavioluism/gestao_vagas/security/SecurityFilter.java
@@ -1,16 +1,24 @@
 package br.com.otavioluism.gestao_vagas.security;
 
+import br.com.otavioluism.gestao_vagas.providers.JWTProvider;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Collections;
 
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JWTProvider jwtProvider;
 
 
     @Override
@@ -18,8 +26,21 @@ public class SecurityFilter extends OncePerRequestFilter {
                                     HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
 
-        String header = request.getHeader("Authorization");
+        SecurityContextHolder.getContext().setAuthentication(null);
 
-        System.out.println(header);
+        String headerAuth = request.getHeader("Authorization");
+
+        if (headerAuth != null) {
+            var subjectToken = this.jwtProvider.validateToken(headerAuth);
+            if (subjectToken.isBlank()) {
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                return;
+            }
+            request.setAttribute("company_id", subjectToken);
+            UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(subjectToken, null, Collections.emptyList());
+            SecurityContextHolder.getContext().setAuthentication(auth); // estamos inserindo para o spring security a variavel auth que nos denota como esta sendo autenticado as rotas
+        }
+
+        filterChain.doFilter(request, response);
     }
 }
