5455a6d6747c35eece282049de331bb472e0d58d
otavioluism <otavioluism97@hotmail.com>
2024-11-04 22:41:27 -0300

    Criando autenticação para a modulo de candidato utilizando o record e também o builder do spring

--- src/main/java/br/com/otavioluism/gestao_vagas/modules/candidate/CandidateRepository.java
+++ src/main/java/br/com/otavioluism/gestao_vagas/modules/candidate/CandidateRepository.java
@@ -8,4 +8,5 @@ import java.util.UUID;
 // poderiamos utilizar a anotation @Repository, mas como JPA já tem dentro dela, ele já sabe se virar que é uma camada que se conecta ao banco de dados
 public interface CandidateRepository extends JpaRepository<CandidateEntity, UUID>  { // cria uma interface que extende da classe JPA onde passamos a entidade que se conecta ao banco e o id da entidade
     Optional<CandidateEntity> findByUsernameOrEmail(String username, String email);
+    Optional<CandidateEntity> findByUsername(String username);
 }
---
+++ src/main/java/br/com/otavioluism/gestao_vagas/modules/candidate/dto/AuthCandidateRequestDTO.java
@@ -0,0 +1,6 @@
+package br.com.otavioluism.gestao_vagas.modules.candidate.dto;
+
+
+// criando uma classe imutavel
+public record AuthCandidateRequestDTO(String username, String password) {
+}
