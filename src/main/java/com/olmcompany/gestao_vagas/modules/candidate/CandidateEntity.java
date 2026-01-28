b5d45a5422462bf3665c3975914457adf33913a0
otavioluism <otavioluism97@hotmail.com>
2026-01-28 07:58:41 -0300

    feature - Adicionando validação para os campos passados pelo usuário (@Valid)

--- src/main/java/com/olmcompany/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/com/olmcompany/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -1,7 +1,10 @@
 package com.olmcompany.gestao_vagas.modules.candidate;
 
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 import java.util.UUID;
 
@@ -9,9 +12,16 @@ import java.util.UUID;
 public class CandidateEntity {
 
     private UUID id;
+
+    @Pattern(regexp = "\\S+", message = "O campo [username] não deve conter espaço!")
     private String username;
+
+    @Length(min = 10, max = 100, message = "A senha deve conter entre (10) e (100) caracteres")
     private String password;
+
+    @Email(message = "O campo [email] deve conter um e-mail válido")
     private String email;
+
     private String name;
     private String description;
     private String curriculum;
