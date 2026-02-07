c17765ceb09a82f5e0e621dd56f546010ea45660
otavioluism <otavioluism97@hotmail.com>
2026-02-07 09:23:44 -0300

    feature - add senha encriptada pelo sistema salvar em base

--- src/main/java/com/olmcompany/gestao_vagas/modules/candidate/useCases/CreateCandidateUseCase.java
+++ src/main/java/com/olmcompany/gestao_vagas/modules/candidate/useCases/CreateCandidateUseCase.java
@@ -3,15 +3,19 @@ package com.olmcompany.gestao_vagas.modules.candidate.useCases;
 import com.olmcompany.gestao_vagas.exceptions.UserFoundException;
 import com.olmcompany.gestao_vagas.modules.candidate.CandidateEntity;
 import com.olmcompany.gestao_vagas.modules.candidate.CandidateRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CreateCandidateUseCase {
 
-    private CandidateRepository candidateRepository;
+    private final CandidateRepository candidateRepository;
 
-    public CreateCandidateUseCase(CandidateRepository candidateRepository){
+    private final PasswordEncoder passwordEncoder;
+
+    public CreateCandidateUseCase(CandidateRepository candidateRepository, PasswordEncoder passwordEncoder){
         this.candidateRepository = candidateRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public CandidateEntity execute(CandidateEntity candidateEntity) {
@@ -19,7 +23,8 @@ public class CreateCandidateUseCase {
                 .ifPresent(candidate -> {
                     throw new UserFoundException("Candidate já existe!");
                 });
-
+        var passwordEncoded = this.passwordEncoder.encode(candidateEntity.getPassword());
+        candidateEntity.setPassword(passwordEncoded);
         System.out.println("Imprimindo informação do candidato:");
         System.out.println(candidateEntity.getName());
         System.out.println(candidateEntity.getUsername());
--- src/main/java/com/olmcompany/gestao_vagas/modules/company/useCases/CreateCompanyUseCase.java
+++ src/main/java/com/olmcompany/gestao_vagas/modules/company/useCases/CreateCompanyUseCase.java
@@ -3,15 +3,19 @@ package com.olmcompany.gestao_vagas.modules.company.useCases;
 import com.olmcompany.gestao_vagas.exceptions.UserFoundException;
 import com.olmcompany.gestao_vagas.modules.company.entities.CompanyEntity;
 import com.olmcompany.gestao_vagas.modules.company.repositories.CompanyRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CreateCompanyUseCase {
 
-    private CompanyRepository companyRepository;
+    private final CompanyRepository companyRepository;
 
-    public CreateCompanyUseCase(CompanyRepository companyRepository){
+    private final PasswordEncoder passwordEncoder;
+
+    public CreateCompanyUseCase(CompanyRepository companyRepository, PasswordEncoder passwordEncoder){
         this.companyRepository = companyRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public CompanyEntity execute(CompanyEntity companyEntity){
@@ -19,7 +23,8 @@ public class CreateCompanyUseCase {
                 .ifPresent((user) -> {
                     throw new UserFoundException("Company já inserido!");
                 });
-
+        var responseEncoded = this.passwordEncoder.encode(companyEntity.getPassword());
+        companyEntity.setPassword(responseEncoded);
         return this.companyRepository.save(companyEntity);
     }
 
