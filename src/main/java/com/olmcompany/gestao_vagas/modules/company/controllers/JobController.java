33fb238fd3850d8c218d3806fb6dca4c0841ecae
otavioluism <otavioluism97@hotmail.com>
2026-02-26 20:49:51 -0300

    feature - criando doc para controller de Vagas e adicionando campos obrigatório do request

--- src/main/java/com/olmcompany/gestao_vagas/modules/company/controllers/JobController.java
+++ src/main/java/com/olmcompany/gestao_vagas/modules/company/controllers/JobController.java
@@ -3,6 +3,14 @@ package com.olmcompany.gestao_vagas.modules.company.controllers;
 import com.olmcompany.gestao_vagas.modules.company.entities.JobEntity;
 import com.olmcompany.gestao_vagas.modules.company.useCases.CreateJobUseCase;
 import com.olmcompany.gestao_vagas.modules.company.dto.JobRequestEntityDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
@@ -26,6 +34,14 @@ public class JobController {
 
     @PostMapping("/")
     @PreAuthorize("hasRole('COMPANY')")
+    @Tag(name = "Vagas", description = "Rotas para controle de vagas") // renomeia a classe do controller
+    @Operation(summary = "Criação de vagas para a empresa!", description = "Essa função é responsável por criar as vagas de uma empresa!") // coloca as descrições sobre a operação em si
+    @ApiResponses(
+            @ApiResponse(responseCode = "200", content = {
+                    @Content(schema = @Schema(implementation = JobEntity.class))
+            })
+    ) // neste insere como será a saída quando der certo, lembrando que pode colocar mais de um
+    @SecurityRequirement(name = "jwt_auth")
     public ResponseEntity create(@Valid @RequestBody JobRequestEntityDTO jobEntityDTO, HttpServletRequest request) {
         try {
             var companyId = request.getAttribute("company_id");
--- src/main/java/com/olmcompany/gestao_vagas/modules/company/dto/JobRequestEntityDTO.java
+++ src/main/java/com/olmcompany/gestao_vagas/modules/company/dto/JobRequestEntityDTO.java
@@ -1,13 +1,19 @@
 package com.olmcompany.gestao_vagas.modules.company.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 
 public class JobRequestEntityDTO {
 
+    @Schema(example = "Vaga JAVA para desenvolvedor júnior!", requiredMode = Schema.RequiredMode.REQUIRED)
     private String description;
+
+    @Schema(example = "GYMpass, DayOff, Plano de Saúde", requiredMode = Schema.RequiredMode.REQUIRED)
     private String benefits;
+
+    @Schema(example = "Vaga JAVA para desenvolvedor júnior!", requiredMode = Schema.RequiredMode.REQUIRED)
     private String level;
 
 }
